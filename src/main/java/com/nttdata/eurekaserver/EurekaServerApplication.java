4f632f3923117a760f33e2de5947a48f27a29685
Joseph Jesus Florian Flores <jflorifl@emeal.nttdata.com>
2026-05-17 12:48:48 -0500

    first commit

---
+++ src/main/java/com/nttdata/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.nttdata.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Main application class for Eureka Server.
+ * Enables Eureka Server for service discovery.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
